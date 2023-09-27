434e743289b701b27ee73d67864aff5b5736ac8a
Matt Darlington <matthewsdarlington@gmail.com>
2023-09-27 11:22:26 -0600

    On headless commit, pushing full changes now. Through date iteration 4. Missing only event#sell.

---
+++ lib/event.rb
@@ -0,0 +1,77 @@
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+require 'date'
+
+class Event
+  attr_reader :name, :food_trucks, :date
+
+  def initialize(name)
+    @name = name
+    @food_trucks = []
+    @date = create_date
+  end
+
+  def add_food_truck(truck)
+    @food_trucks << truck
+  end
+
+  def food_truck_names
+    @food_trucks.map do |truck|
+      truck.name
+    end
+  end
+
+  def food_trucks_that_sell(item_str)
+    @food_trucks.find_all do |truck|
+      truck.check_stock(item_str) > 0
+    end
+  end
+
+  def get_item_qty(item)
+    count = 0
+    food_trucks_that_sell(item).each do |truck|
+      count += truck.check_stock(item)
+    end
+    count
+  end
+
+  def total_inventory
+    item_qty_trucks = {}
+    @food_trucks.each do |truck|
+      truck.inventory.each do |item, qty|
+        qty_trucks = {
+          quantity: get_item_qty(item),
+          food_trucks: food_trucks_that_sell(item)
+        }
+        item_qty_trucks[item] = qty_trucks
+      end
+    end
+    item_qty_trucks
+  end
+
+  def overstocked_items
+    overstocked_items = []
+    total_inventory.each do |item, qty_trucks_hash|
+      if  qty_trucks_hash[:quantity] > 50
+        overstocked_items << item
+      end
+    end
+    overstocked_items
+  end
+
+  def sorted_items_list
+    sorted_item_names = total_inventory.keys.sort do |item1, item2|
+      item1.name <=> item2.name
+    end.map do |item|
+      item.name
+    end
+    sorted_item_names
+  end
+
+  def create_date
+    today = Date.today
+    today.strftime('%d/%m/%Y')
+  end
+end
+
